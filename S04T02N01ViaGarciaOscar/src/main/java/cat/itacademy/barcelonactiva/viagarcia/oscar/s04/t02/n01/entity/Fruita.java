fd6aef9f892d874460a957d1c2a67b337b62f411
Oscar <oscar.via@gmail.com>
2022-07-13 14:36:48 +0200

    Primera Tramesa Nivell1

---
+++ S04T02N01ViaGarciaOscar/src/main/java/cat/itacademy/barcelonactiva/viagarcia/oscar/s04/t02/n01/entity/Fruita.java
@@ -0,0 +1,56 @@
+package cat.itacademy.barcelonactiva.viagarcia.oscar.s04.t02.n01.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="fruita")
+public class Fruita {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "nom")
+	private String nom;
+	
+	@Column(name = "quantitat")
+	private int quantitatQuilos;
+	
+	public Fruita() {}
+
+	public Fruita(String nom, int quantitatQuilos) {
+		this.nom = nom;
+		this.quantitatQuilos = quantitatQuilos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getQuantitatQuilos() {
+		return quantitatQuilos;
+	}
+
+	public void setQuantitatQuilos(int quantitatQuilos) {
+		this.quantitatQuilos = quantitatQuilos;
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruita [id=" + id + ", nom=" + nom + ", quantitat=" + quantitatQuilos + "]";
+	}	
+
+}
---
+++ S04T02N01ViaGarciaOscar/src/main/java/cat/itacademy/barcelonactiva/viagarcia/oscar/s04/t02/n01/model/repository/FruitaRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.viagarcia.oscar.s04.t02.n01.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cat.itacademy.barcelonactiva.viagarcia.oscar.s04.t02.n01.entity.Fruita;
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer>{
+
+	
+}
